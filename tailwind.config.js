79ff19fceb121fc6c9cfd98114287942db8e190a
Shivam Mishra <scm.mymail@gmail.com>
2020-02-20 10:41:44 +0530

    feat: configure tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,27 @@
 module.exports = {
-  theme: {
-    extend: {},
-  },
-  variants: {},
-  plugins: [],
-}
+	theme: {
+		extend: {},
+		display: ["IBM Plex Sans", "sans-serif"],
+		body: ["IBM Plex Sans", "sans-serif"],
+		fontSize: {
+			xs: ".75rem",
+			sm: ".875rem",
+			base: "1rem",
+			lg: "1.125rem",
+			xl: "1.25rem",
+			"2xl": "1.5rem",
+			"3xl": "1.875rem",
+			"4xl": "2.25rem",
+			"5xl": "3rem",
+			"6xl": "4rem",
+			"7xl": "5rem",
+			"8xl": "6rem"
+		},
+		container: {
+    		center: true,
+    		padding: '2rem',
+    	},
+	},
+	variants: {},
+	plugins: []
+};
